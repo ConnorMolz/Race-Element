fc4cf480c340d8dce2b94a83bfc9b055cc76b6f1
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-12-25 01:58:52 +0100

    Update StartScreenOverlay.cs
    - removed tweening
    - added double text tendering

--- Race_Element.HUD.ACC/Overlays/System/OverlayStartScreen/StartScreenOverlay.cs
+++ Race_Element.HUD.ACC/Overlays/System/OverlayStartScreen/StartScreenOverlay.cs
@@ -6,7 +6,6 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Text;
-using Unglide;
 
 namespace RaceElement.HUD.ACC.Overlays.OverlayStartScreen;
 
@@ -24,16 +23,20 @@ public sealed class StartScreenOverlay : AbstractOverlay
     private CachedBitmap _cachedBackground;
     private CachedBitmap _cachedText;
     private CachedBitmap _slider;
-    private Tweener tweener;
     private Stopwatch stopwatch;
 
     private const int SliderWidth = 280;
+    private const float SliderMargin = 8f;
     private int sliderX = -SliderWidth;
 
     public StartScreenOverlay(Rectangle rectangle) : base(rectangle, "Start Screen")
     {
-        this.X = rectangle.X;
-        this.Y = rectangle.Y;
+        if (rectangle.X != 0 || rectangle.Y != 0) // if there's no preset, lets not set it.
+        {
+            this.X = rectangle.X;
+            this.Y = rectangle.Y;
+        }
+
         this.Width = 620;
         this.Height = 74;
         this.RefreshRateHz = 60;
@@ -42,24 +45,26 @@ public sealed class StartScreenOverlay : AbstractOverlay
 
     public override void BeforeStart()
     {
-        _clippingPath = GraphicsExtensions.CreateRoundedRectangle(new(1, 0, Width - 2, Height - 1), 8, 8, 8, 8);
+
+        float rounding = 19f;
+        int intRounding = (int)rounding;
+        _clippingPath = GraphicsExtensions.CreateRoundedRectangle(new(1, 0, Width - 2, Height - 1), intRounding, intRounding, intRounding, intRounding);
 
         _cachedBackground = new CachedBitmap(this.Width, this.Height, g =>
         {
-            g.CompositingMode = CompositingMode.SourceCopy;
             Rectangle rectangle = new(0, 0, Width - 1, Height - 1);
-            using HatchBrush hatchBrush = new(HatchStyle.LightUpwardDiagonal, Color.FromArgb(255, Color.Black), Color.FromArgb(230, Color.Black));
-
-            g.FillRoundedRectangle(hatchBrush, rectangle, 8);
-
+            using LinearGradientBrush solidBackgroundBrush = new(PointF.Empty, new PointF(Width, Height), Color.FromArgb(255, 0, 0, 0), Color.FromArgb(8, 255, 0, 0));
+            using HatchBrush hatchBrush = new(HatchStyle.LightUpwardDiagonal, Color.FromArgb(2, Color.Red), Color.FromArgb(230, Color.Black));
+            g.FillRoundedRectangle(solidBackgroundBrush, rectangle, intRounding);
+            g.FillRoundedRectangle(hatchBrush, rectangle, intRounding);
 
             int crimpAmount = -1;
             Rectangle crimpRect = new(rectangle.X + crimpAmount, rectangle.Y + crimpAmount, rectangle.Width - crimpAmount * 2, rectangle.Height - crimpAmount * 2);
 
-            using SolidBrush additionalBrush = new(Color.FromArgb(50, Color.Red));
-            using Pen additionalPen = new(additionalBrush, 1.6f);
+            using SolidBrush additionalBrush = new(Color.FromArgb(8, Color.OrangeRed));
+            using Pen additionalPen = new(additionalBrush, 5f);
             g.DrawLine(additionalPen, new(0, 0), new(Width, 0));
-        }, opacity: 0);
+        }, opacity: 1f);
 
         _cachedText = new CachedBitmap(this.Width, this.Height, g =>
         {
@@ -75,26 +80,30 @@ public sealed class StartScreenOverlay : AbstractOverlay
             Font font16 = FontUtil.FontConthrax(32);
             int font16Height = (int)font16.GetHeight(g);
             int halfStringLength = (int)(g.MeasureString(header, font16).Width / 2);
-            int x = this.Width / 2 - halfStringLength;
-            g.DrawStringWithShadow(header, font16, Color.FromArgb(215, Color.White), new Point(x, 0), 4f, new StringFormat() { LineAlignment = StringAlignment.Near });
+            int x = this.Width / 2 - halfStringLength - 1;
+
+            g.DrawStringWithShadow(header, font16, Color.FromArgb(170, Color.Red), new PointF(x + 2.7f, 2.3f), 7f, new StringFormat() { LineAlignment = StringAlignment.Near });
+            g.DrawStringWithShadow(header, font16, Color.FromArgb(215, Color.White), new Point(x, 0), 5f, new StringFormat() { LineAlignment = StringAlignment.Near });
+
             font16.Dispose();
 
             transform.Shear(0.3f, 0);
             transform.Translate(-12f, -3f);
             g.Transform = transform;
-            string subHeader = "Developed by Reinier Klarenberg";
-            Font font11 = FontUtil.FontConthrax(13);
-            g.DrawStringWithShadow(subHeader, font11, Color.FromArgb(185, Color.White), new Point(x + 6, font16Height - 2));
+            string subHeader = $"Solutions for Simulators © 2022 - {DateTime.UtcNow.Year} Reinier Klarenberg";
+            Font font11 = FontUtil.FontConthrax(11);
+            g.DrawStringWithShadow(subHeader, font11, Color.FromArgb(185, Color.Red), new PointF(x + 9f, font16Height + 1));
+            g.DrawStringWithShadow(subHeader, font11, Color.FromArgb(185, Color.White), new PointF(x + 8f, font16Height));
             font11.Dispose();
-        }, opacity: 0);
+        }, opacity: 1);
 
         _slider = new CachedBitmap(SliderWidth, Height, g =>
         {
-            RectangleF rect = new(0, 0, SliderWidth, Height / 1.5f);
+            RectangleF rect = new(0, 0, SliderWidth, Height / 1.6f);
             using GraphicsPath gradientPath = new();
             gradientPath.AddEllipse(rect);
             using PathGradientBrush pthGrBrush = new(gradientPath);
-            pthGrBrush.SurroundColors = [Color.FromArgb(40, 255, 0, 0)];
+            pthGrBrush.SurroundColors = [Color.FromArgb(45, 255, 0, 0)];
             pthGrBrush.CenterColor = Color.FromArgb(255, 255, 0, 0);
 
             // draw diagonal lines
@@ -107,20 +116,19 @@ public sealed class StartScreenOverlay : AbstractOverlay
                 g.DrawLine(pen, new Point(baseX, 0), new Point(baseX + SliderWidth / 2, Height));
             }
 
-            float width = 45f;
+            float width = 28f;
             // draw top horizontal line
             PointF topLeft = new(SliderWidth / 2 - width / 2, 0);
             PointF topRight = new(SliderWidth / 2 + width / 2, 0);
-            using SolidBrush topLineBrush = new(Color.FromArgb(70, 255, 0, 0));
+            PointF innerTopLeft = new(SliderWidth / 2 - width / 7f, 0);
+            PointF innerTopRight = new(SliderWidth / 2 + width / 7f, 0);
+
+            using SolidBrush topLineBrush = new(Color.FromArgb(90, 255, 0, 0));
             using Pen topLinePen = new(topLineBrush, 2f);
             g.DrawLine(topLinePen, topLeft, topRight);
-        }, opacity: 0);
+            g.DrawLine(topLinePen, innerTopLeft, innerTopRight);
+        }, opacity: 1);
 
-        tweener = new Tweener();
-        tweener.AddTween(tweener.Tween(_cachedBackground, new { Opacity = 1f }, 2f).Ease(Ease.SineIn));
-        tweener.AddTween(tweener.Tween(_cachedText, new { Opacity = 0.95f }, 1.3f).Ease(Ease.ExpoIn));
-        tweener.AddTween(tweener.Tween(_slider, new { Opacity = 1f }, 2f).Ease(Ease.ExpoIn));
-        stopwatch = Stopwatch.StartNew();
     }
 
     public override void BeforeStop()
@@ -137,17 +145,14 @@ public sealed class StartScreenOverlay : AbstractOverlay
 
     public override void Render(Graphics g)
     {
-        tweener?.Update(stopwatch.ElapsedMilliseconds / 1000f);
-        g.SmoothingMode = SmoothingMode.HighQuality;
-
         g.SetClip(_clippingPath);
 
         _cachedBackground?.Draw(g);
 
-        if (sliderX > Width)
+        if (sliderX > Width - SliderMargin)
             sliderX = -SliderWidth;
         _slider?.Draw(g, new Point(sliderX, 0));
-        sliderX += 8;
+        sliderX += 16;
 
         _cachedText?.Draw(g);
     }
